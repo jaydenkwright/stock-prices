305c8aa666a02b7f0ffc3abc5d6b47c54f70e93d
jaydenkwright <jaydenkwright3@gmail.com>
2020-02-12 23:22:30 -0500

    Updates

---
+++ precache-manifest.a58c157aaebc36d521423e183e3abb58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a637785d23510a8349ae1c44c4a294b6",
+    "url": "/stock-prices/index.html"
+  },
+  {
+    "revision": "733c02b9bb89d02bcfe2",
+    "url": "/stock-prices/static/css/main.36430d9a.chunk.css"
+  },
+  {
+    "revision": "851d1c2adb78f98bd966",
+    "url": "/stock-prices/static/js/2.5e935b05.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/stock-prices/static/js/2.5e935b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "733c02b9bb89d02bcfe2",
+    "url": "/stock-prices/static/js/main.cfe9d9c9.chunk.js"
+  },
+  {
+    "revision": "432a8709f0bc0e1fd2b1",
+    "url": "/stock-prices/static/js/runtime-main.d52a3360.js"
+  }
+]);
