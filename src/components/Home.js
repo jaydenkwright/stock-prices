08c35e271e2b92c6f2e6c550bbf2d682555402af
jaydenkwright <jaydenkwright3@gmail.com>
2020-02-14 23:09:09 -0500

    Added homepage page load transitions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import {useSpring, animated} from 'react-spring'
 import styles from './Home.module.css'
 
 class Home extends Component {
     state = {
         data: [],
-        news: []
+        news: [],
+        isLoaded: false,
     }
     componentDidMount(){
         fetch(`https://cloud.iexapis.com/stable/stock/${this.props.stockName}/quote?token=${process.env.REACT_APP_STOCK_KEY}`)
             .then(res => res.json())
             .then(result => {
                 this.setState({
-                    data: result
+                    data: result,
+                    isLoaded: true
                 })
             });
         fetch(`https://newsapi.org/v2/everything?q=${this.props.stockName}&apiKey=${process.env.REACT_APP_NEWS_KEY}`)
@@ -30,21 +33,24 @@ class Home extends Component {
     componentWillUnmount(){
         document.body.style.backgroundColor = null;
     }
+
 render(){
+
         return (
+            
             <div>
                 <div className={styles.infoBox}>
-                    <div className={styles.title}>
+                    <div className={this.state.isLoaded === true ? styles.title : ''}>
                         <h3>{this.state.data.symbol}</h3>
                     </div>
                     <h4>
-                        <div className={styles.info}>
+                        <div className={this.state.isLoaded === true ? styles.info: ''}>
                             <p>{this.state.data.companyName}</p>
                             <p>${this.state.data.latestPrice}</p>
                             <p>{this.state.data.primaryExchange}</p>
                         </div>
                     </h4>
-                    <div className={styles.news}>
+                    <div className={this.state.isLoaded === true ? styles.news : ''}>
                         <h1>{this.state.news.title}</h1>
                         <p>{this.state.news.description}</p>
                     </div>
